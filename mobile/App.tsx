21e411f84ecd69cb303bb29e1072bdb4618aed71
juninho15830 <juninho15830@gmail.com>
2023-11-28 15:34:34 -0300

    update tailwind in mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 font-bold text-5xl">Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
