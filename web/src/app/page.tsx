7c8fdb01c9db676005b830794babf7da5a9cdf64
juninho15830 <juninho15830@gmail.com>
2023-12-02 15:50:13 -0300

    Update CSS web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="text-4xl font-bold font-alt">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10">
+
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 -translate-y-1/2 rounded-full translate-x-1/2 blur-full"/>
+
+        {/* Stripes */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>!</p>
+        </div>
+      </div>
+    </main>
   )
 }
