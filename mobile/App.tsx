8d25fed037a74f222b99cf000a90cf1f9c21e5dc
juninho15830 <juninho15830@gmail.com>
2023-12-05 10:38:13 -0300

    update mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">ARTHUR</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl">ARTHUR</Text>
       <StatusBar style="light" translucent />
     </View>
   )
