b1405d1982ee8a9e7f0031e32b8cdee11fc4f987
juninho15830 <32220266+juninho15830@users.noreply.github.com>
2023-12-06 15:38:07 -0300

    update db routes

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/users', async () => {
+    const users = await prisma.user.findMany()
+
+    return users
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,9 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
