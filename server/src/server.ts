dc5f5c2b13927123ba507df92fe81ef8266e5420
juninho15830 <32220266+juninho15830@users.noreply.github.com>
2023-12-22 13:33:20 -0300

    Update upload back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+// Configurar pasta uplodas como pública
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'), // Caminho da pasta
+  prefix: '/uploads', // Prefixo da url antes do nome do arquivo
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +29,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
